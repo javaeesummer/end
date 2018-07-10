5e9b58d827e3ea6ae31d11a554e897f17dc394d2
Pissssofshit <1412143367@qq.com>
2018-07-10 08:47:08 +0800

    requestmapping

--- rev-contorller/src/main/java/com/rev/revcontorller/service/test.java
+++ rev-contorller/src/main/java/com/rev/revcontorller/service/test.java
@@ -1,15 +1,17 @@
 package com.rev.revcontorller.service;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.rev.facade.st;
 
 @RestController
+@RequestMapping("/test")
 public class test {
 
     @Reference(version = "1.0.0")
     private st sts;
-
+    @RequestMapping("/sayhello")
     public void test(){
         sts.sayhello();
     }
