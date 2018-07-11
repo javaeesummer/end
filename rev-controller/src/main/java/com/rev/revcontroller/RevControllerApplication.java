953f618efdfb9bd27e26c79c69b7ada87b4d8d83
Pissssofshit <1412143367@qq.com>
2018-07-11 20:38:31 +0800

    new module

---
+++ rev-controller/src/main/java/com/rev/revcontroller/RevControllerApplication.java
@@ -0,0 +1,12 @@
+package com.rev.revcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevControllerApplication.class, args);
+	}
+}
