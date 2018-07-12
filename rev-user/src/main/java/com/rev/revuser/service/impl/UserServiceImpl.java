24dda043d447d236190a5439360280a433cac5a3
Pissssofshit <1412143367@qq.com>
2018-07-12 16:10:55 +0800

    user part service

--- rev-user/src/main/java/com/rev/revuser/service/impl/UserServiceImpl.java
+++ rev-user/src/main/java/com/rev/revuser/service/impl/UserServiceImpl.java
@@ -2,7 +2,11 @@ package com.rev.revuser.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 
+import com.rev.revuser.bean.sponsorBean;
 import com.rev.revuser.dao.UserMapper;
+import com.rev.revuser.param.loginParam;
+import com.rev.revuser.param.registerJudgeParam;
+import com.rev.revuser.param.registerParam;
 import com.rev.revuser.result.UserView;
 import com.rev.revuser.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +18,32 @@ import java.util.List;
 public class UserServiceImpl  implements UserService {
     @Resource
     UserMapper userMapper;
+
+    @Override
+    public String login(loginParam loginParam) {
+        return null;
+    }
+
+    @Override
+    public sponsorBean toHoldActivity(UserView userView) {
+        return null;
+    }
+
+    @Override
+    public String register(registerParam registerParam) {
+        return null;
+    }
+
+    @Override
+    public String registerJudge(registerJudgeParam registerJudgeParam) {
+        return null;
+    }
+
+    @Override
+    public String usertoJudge(registerJudgeParam registerJudgeParam) {
+        return null;
+    }
+
     @Override
     public List<UserView> getAllUser() {
         return userMapper.getAllUser();
