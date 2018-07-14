db23d814771ce950af5e0a5c6c77c6104833da67
ZSKZSKZSKZSK <zhusaikui@163.com>
2018-07-14 14:28:16 +0800

    testfile

---
+++ rev-file/src/main/java/com/rev/revfile/dao/WorksMapper.java
@@ -0,0 +1,20 @@
+package com.rev.revfile.dao;
+
+import com.rev.revfile.param.ActivityParam;
+import com.rev.revfile.param.FileParam;
+import com.rev.revfile.result.FileResult;
+
+import java.util.List;
+
+public interface WorksMapper {
+
+    //修改作品信息
+    void updateWorksByAttendorid(FileParam fileParam);
+    //删除作品
+    void deleteWorksByAttendorid(Integer attendorid);
+    //显示某个参赛者作品
+    FileResult getFileByAttendorId (Integer attendorid);
+    //显示当前活动的所有作品
+    List<FileResult> getAllFile(Integer activityid);
+
+}
