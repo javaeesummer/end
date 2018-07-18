349d2d2d3dd4072d884883699dbf8e6884655781
周骏迪 <92169097@qq.com>
2018-07-18 19:44:46 +0800

    解决跨域

--- rev-controller/src/main/java/com/rev/revcontroller/controller/judgeController.java
+++ rev-controller/src/main/java/com/rev/revcontroller/controller/judgeController.java
@@ -18,6 +18,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @Component("judgeService")
 @RequestMapping("/judge")
--- rev-controller/src/main/java/com/rev/revcontroller/controller/userController.java
+++ rev-controller/src/main/java/com/rev/revcontroller/controller/userController.java
@@ -23,6 +23,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 
+@CrossOrigin
 @RestController
 @Component("userService")
 @RequestMapping("/user")
