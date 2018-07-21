e384707228d7ceca1607699417e359b40bbb9f76
Pissssofshit <1412143367@qq.com>
2018-07-21 19:18:25 +0800

    Merge branch 'master' of /home/hxs/作业/大数据/end with conflicts.

--- rev-file/src/main/java/com/rev/revfile/service/imp/FileServiceImp.java
+++ rev-file/src/main/java/com/rev/revfile/service/imp/FileServiceImp.java
@@ -1,51 +1,51 @@
-package com.rev.revfile.service.imp;
-
-import com.alibaba.dubbo.config.annotation.Service;
-import com.rev.revfile.param.ActivityParam;
-import com.rev.revfile.param.FileParam;
-import com.rev.revfile.result.FileResult;
-import com.rev.revfile.service.FileService;
-import com.rev.revfile.dao.WorksMapper;
-
-
-import javax.annotation.Resource;
-import java.util.List;
-@Service
-public class FileServiceImp implements FileService {
-    @Resource
-    WorksMapper worksmapper;
-    //参赛者上传作品
-    @Override
-    public void uploadFile(FileParam file) {
-        worksmapper.insertFile(file);
-    }
-    //删除作品
-    @Override
-    public void deleteFile(FileParam file) {
-    worksmapper.deleteWorksByAttendorid(file.getAttendorid());
-    }
-    //修改作品
-    @Override
-    public void moddifyFile(FileParam file) {
-         worksmapper.updateWorksByAttendorid(file);
-    }
-    @Override
-    public void moddifyNoFile(FileParam file){
-        worksmapper.updateWorksByAttendoridNofile(file);
-    }
-    //显示所有作品
-    @Override
-    public List<FileResult> getAllFile(ActivityParam activity) {
-        return worksmapper.getAllFile(activity.getAcctivityId());
-    }
-    //显示某个参赛者作品
-    @Override
-    public FileResult getFileByAttendorId(FileParam file) {
-        return worksmapper.getFileByAttendorId(file.getAttendorid());
-    }
-    //下载参赛者作品
-    @Override
-    public FileResult dowmloadFile(FileParam file) {
-        return null;
-    }
-}
+//package com.rev.revfile.service.imp;
+//
+//import com.alibaba.dubbo.config.annotation.Service;
+//import com.rev.revfile.param.ActivityParam;
+//import com.rev.revfile.param.FileParam;
+//import com.rev.revfile.result.FileResult;
+//import com.rev.revfile.service.FileService;
+//import com.rev.revfile.dao.WorksMapper;
+//
+//
+//import javax.annotation.Resource;
+//import java.util.List;
+//@Service
+//public class FileServiceImp implements FileService {
+//    @Resource
+//    WorksMapper worksmapper;
+//    //参赛者上传作品
+//    @Override
+//    public void uploadFile(FileParam file) {
+//        worksmapper.insertFile(file);
+//    }
+//    //删除作品
+//    @Override
+//    public void deleteFile(FileParam file) {
+//    worksmapper.deleteWorksByAttendorid(file.getAttendorid());
+//    }
+//    //修改作品
+//    @Override
+//    public void moddifyFile(FileParam file) {
+//         worksmapper.updateWorksByAttendorid(file);
+//    }
+//    @Override
+//    public void moddifyNoFile(FileParam file){
+//        worksmapper.updateWorksByAttendoridNofile(file);
+//    }
+//    //显示所有作品
+//    @Override
+//    public List<FileResult> getAllFile(ActivityParam activity) {
+//        return worksmapper.getAllFile(activity.getAcctivityId());
+//    }
+//    //显示某个参赛者作品
+//    @Override
+//    public FileResult getFileByAttendorId(FileParam file) {
+//        return worksmapper.getFileByAttendorId(file.getAttendorid());
+//    }
+//    //下载参赛者作品
+//    @Override
+//    public FileResult dowmloadFile(FileParam file) {
+//        return null;
+//    }
+//}
